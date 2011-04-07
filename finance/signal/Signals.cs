aea3fe9ac8d0a59f9c37abe65383134a8d60ce98
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-07 13:50:03 +0400

    + Signals class

---
+++ finance/signal/Signals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace org.sherman.ru.finance.signal {
+	/// <summary>
+	/// Common stuff
+	/// </summary>
+	/// 
+	
+	sealed class Signals {
+		public const string LONG_OPEN = "LO";
+		public const string SHORT_OPEN = "SO";
+		public const string TAKE_PROFIT_CLOSE = "TP";
+		public const string STOP_LOSS_CLOSE = "SL";
+		public const string TIME_CLOSE = "TC";
+		public const string CLOSE = "C";
+	}
+}
