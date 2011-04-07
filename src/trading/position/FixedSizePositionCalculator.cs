8830226f61577cd109ecad30d88b03941231cbce
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-07 15:37:26 +0400

    + position tools

---
+++ src/trading/position/FixedSizePositionCalculator.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sherminator
+ * Date: 07.04.2011
+ * Time: 15:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace org.ontslab.trading.position {
+	/// <summary>
+	/// Description of FixedSizePositionCalculator.
+	/// </summary>
+	public class FixedSizePositionCalculator : PositionCalculator {
+		private double size;
+		
+		public FixedSizePositionCalculator(double size) { this.size = size; }
+		
+		public double getPositionSize() {
+			return size;
+		}
+	}
+}
---
+++ src/trading/position/PositionCalculator.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sherminator
+ * Date: 07.04.2011
+ * Time: 15:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace org.ontslab.trading.position {
+	/// <summary>
+	/// Description of PositionCalculator.
+	/// </summary>
+	public interface PositionCalculator {
+		double getPositionSize();
+	}
+}
