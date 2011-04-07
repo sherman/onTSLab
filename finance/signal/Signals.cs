78eb3ef66f556cb9e2ef119d9dc10ff74a5176bb
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-07 13:54:18 +0400

    * it is public class

--- finance/signal/Signals.cs
+++ finance/signal/Signals.cs
@@ -6,7 +6,7 @@ namespace org.sherman.ru.finance.signal {
 	/// </summary>
 	/// 
 	
-	sealed class Signals {
+	public sealed class Signals {
 		public const string LONG_OPEN = "LO";
 		public const string SHORT_OPEN = "SO";
 		public const string TAKE_PROFIT_CLOSE = "TP";
