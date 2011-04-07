ecffe85d9517b701f233a222ca9569580477c996
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-07 15:41:24 +0400

    + position calculators factory

---
+++ src/trading/position/Positions.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sherminator
+ * Date: 07.04.2011
+ * Time: 15:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace org.ontslab.trading.position {
+	/// <summary>
+	/// Description of Positions.
+	/// </summary>
+	public static class Positions {
+		public static PositionCalculator fixedSize(double size) {
+			return new FixedSizePositionCalculator(size);
+		}
+	}
+}
