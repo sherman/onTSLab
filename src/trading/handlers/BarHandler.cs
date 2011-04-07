17a86f66297d10feb718d4fd0af1c03dd347a352
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-07 14:10:06 +0400

    + bar handlers starts here

---
+++ src/trading/handlers/BarHandler.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sherminator
+ * Date: 07.04.2011
+ * Time: 13:55
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using TSLab.Script;
+
+namespace org.ontslab.trading.handlers
+{
+	/// <summary>
+	/// Description of BarHandler.
+	/// </summary>
+	public interface BarHandler {
+		void handleBar(Bar bar);
+	}
+}
---
+++ src/trading/handlers/CompositeBarHandler.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sherminator
+ * Date: 07.04.2011
+ * Time: 14:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using TSLab.Script;
+
+namespace org.ontslab.trading.handlers
+{
+	/// <summary>
+	/// Description of CompositeBarHandler.
+	/// </summary>
+	public class CompositeBarHandler : BarHandler {
+		private List<BarHandler> handlers = new List<BarHandler>();
+		
+		public CompositeBarHandler() {}
+		
+		public CompositeBarHandler add(BarHandler handler) {
+			this.handlers.Add(handler);
+			return this;
+		}
+		
+		public void handleBar(Bar bar) {
+			foreach (BarHandler handler in handlers) {
+				handler.handleBar(bar);
+			}
+		}
+	}
+}
