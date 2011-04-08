758741ca714973e7ab198a478c2901ec66a9aa65
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-08 17:38:46 +0400

    + data utils starts here

---
+++ src/data/DataUtils.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sherminator
+ * Date: 08.04.2011
+ * Time: 17:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace org.ontslab.data {
+	/// <summary>
+	/// Description of DataUtils.
+	/// </summary>
+	public static class DataUtils {
+		public static bool isIndicatorGrows(
+			IList<double> data,
+			int barIndex,
+			int bars,
+			double tresholdValue
+		) {
+			if (barIndex - bars < 0) {
+				throw new NotEnoughDataException("Not enough data!");
+			}
+			
+			for (int i = 0; i < bars; i++) {
+				double currValue = data[barIndex - i];
+				double prevValue = data[barIndex - (i + 1)];
+				
+				if (prevValue >= currValue || (currValue - prevValue) < tresholdValue)
+					return false;
+			}
+			
+			return true;
+		}
+	}
+}
---
+++ src/data/NotEnoughDataException.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sherminator
+ * Date: 08.04.2011
+ * Time: 17:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace org.ontslab.data
+{
+	/// <summary>
+	/// Description of NotEnoughDataException.
+	/// </summary>
+	public class NotEnoughDataException : Exception {
+		public NotEnoughDataException(string message) : base(message) {}
+	}
+}
