353e9c1dd17a73b2988a8622ed828562efe4b3e1
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-10 21:30:48 +0400

    + data helpers test added

---
+++ test/data/DataHelpersTest.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sherminator
+ * Date: 10.04.2011
+ * Time: 21:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using org.ontslab.data;
+
+namespace org.ontslab.test.data {
+	[TestFixture]
+	public class DataHelpersTest {
+		[Test]
+		public void testHighest() {
+			IList<double> data = new List<double>{1, 2, 6, 1};
+			
+			Assert.AreEqual(2, DataHelpers.highest(0, data, 2));
+			Assert.AreEqual(6, DataHelpers.highest(0, data, 10));
+			Assert.AreEqual(1, DataHelpers.highest(3, data, 10));
+		}
+	}
+}
