f9a400b02fe42a45f77b2603cf70fe0dce1ed351
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-10 18:32:59 +0400

    * extend security methods

---
+++ src/data/Source.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sherminator
+ * Date: 10.04.2011
+ * Time: 18:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using TSLab.Script;
+
+namespace org.ontslab.data {
+	/// <summary>
+	/// Description of SourceUtils.
+	/// </summary>
+	
+	public static class SecurityExtensions {
+        public static bool isLastTradeLoss(this ISecurity source) {
+            IPosition lastPosition = source.Positions.LastPositionClosed;
+			
+			if (null == lastPosition)
+				return false;
+			
+			return lastPosition.Profit() < 0;
+        }
+    }   
+}
