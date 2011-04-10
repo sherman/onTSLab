1d37cd6f3575066ad7f01c9febcbf0c467346278
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-10 20:31:00 +0400

    + cutomisable convertion method

--- src/trading/handlers/BalanceBarHandler.cs
+++ src/trading/handlers/BalanceBarHandler.cs
@@ -13,10 +13,15 @@ namespace org.ontslab.trading.handlers {
 	/// <summary>
 	/// Description of BalanceBarHandler.
 	/// </summary>
+	/// 
+	
+	public delegate double BalanceConvertion(IPosition position);
+	
 	public class BalanceBarHandler : BarHandler {
 		private double balance = 0.0;
 		private Bar lastBar;
 		private ISecurity source;
+		private BalanceConvertion method;
 		
 		public BalanceBarHandler(ISecurity source) {
 			this.source = source;
@@ -26,13 +31,22 @@ namespace org.ontslab.trading.handlers {
 			IPosition previousPosition = source.Positions.LastPositionClosed;
 			
 			if (previousPosition != null && (lastBar == null || !previousPosition.ExitBar.Equals(lastBar))) {
-				balance += previousPosition.Profit();
+				if (null != method) {
+					balance += method(previousPosition);
+				} else {
+					balance += previousPosition.Profit();
+				}
 				lastBar = previousPosition.ExitBar;
 			}
 		}
 		
 		public double getBalance() {
 			return balance;
+		}
+		
+		public BalanceBarHandler setConvertionMethod(BalanceConvertion method) {
+			this.method = method;
+			return this;
 		}
 	}
 }
