ad2de2c12922de988556887218479b27cffc3642
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-10 21:32:26 +0400

    + one more test

--- test/data/DataHelpersTest.cs
+++ test/data/DataHelpersTest.cs
@@ -12,6 +12,8 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using org.ontslab.data;
 
+// TODO: use parameters
+
 namespace org.ontslab.test.data {
 	[TestFixture]
 	public class DataHelpersTest {
@@ -22,6 +24,16 @@ namespace org.ontslab.test.data {
 			Assert.AreEqual(2, DataHelpers.highest(0, data, 2));
 			Assert.AreEqual(6, DataHelpers.highest(0, data, 10));
 			Assert.AreEqual(1, DataHelpers.highest(3, data, 10));
+		}
+		
+		[Test]
+		public void testLowest() {
+			IList<double> data = new List<double>{1, 2, 6, 1};
+			
+			Assert.AreEqual(1, DataHelpers.lowest(0, data, 2));
+			Assert.AreEqual(1, DataHelpers.lowest(0, data, 10));
+			Assert.AreEqual(1, DataHelpers.lowest(3, data, 10));
+			Assert.AreEqual(2, DataHelpers.lowest(1, data, 1));
 		}
 	}
 }
