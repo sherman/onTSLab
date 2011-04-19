cf6f9b41f4b8a552427f1df0cdd3cd7f97b0d616
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-19 18:04:42 +0400

    + hourly based source (still a lot of copypaste)

---
+++ src/data/HourlySource.cs
@@ -0,0 +1,84 @@
+﻿/***************************************************************************
+*   Copyright (C) 2011 by Denis M. Gabaydulin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License as        *
+*   published by the Free Software Foundation; either version 3 of the    *
+*   License, or (at your option) any later version.                       *
+*                                                                         *
+***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TSLab.Script;
+using TSLab.DataSource;
+
+namespace org.ontslab.data {
+	/// <summary>
+	/// Description of HourlySource.
+	/// </summary>
+	public class HourlySource {
+		private IDictionary<string, Bar> hourlySource;
+		private DateTime first;
+		private DateTime last;
+		
+		public HourlySource(IList<Bar> original) {
+			createHourlySourceFrom(original);
+		}
+		
+		public Bar getBar(DateTime date) {
+			return hourlySource[date.ToShortDateString() + "_" + date.Hour.ToString()];
+		}
+		
+		public Bar getPreviousBar(DateTime date) {
+			string previousBarKey = null;
+			
+			DateTime startBarDate = date;
+			
+			do {
+				startBarDate = startBarDate.Subtract(new TimeSpan(0, 1, 0, 0, 0));
+				previousBarKey = startBarDate.ToShortDateString() + "_" + startBarDate.Hour.ToString();
+				
+				if (hourlySource.ContainsKey(previousBarKey)) {
+					break;
+				}
+			} while (startBarDate >= first);
+			
+			if (hourlySource.ContainsKey(previousBarKey))
+				return hourlySource[previousBarKey];
+			else
+				return null;
+		}
+		
+		public Bar getNextBar(DateTime date) {
+			string nextBarKey = null;
+			
+			DateTime startBarDate = date;
+			
+			do {
+				startBarDate = startBarDate.Add(new TimeSpan(0, 1, 0, 0, 0));
+				nextBarKey = startBarDate.ToShortDateString() + "_" + startBarDate.Hour.ToString();
+				
+				if (hourlySource.ContainsKey(nextBarKey)) {
+					break;
+				}
+			} while (startBarDate <= last);
+			
+			if (hourlySource.ContainsKey(nextBarKey))
+				return hourlySource[nextBarKey];
+			else
+				return null;
+		}
+		
+		private void createHourlySourceFrom(IList<Bar> source) {
+			hourlySource = new Dictionary<string, Bar>(source.Count);
+			
+			source.ToList().ForEach(
+				bar => hourlySource.Add(bar.Date.ToShortDateString() + "_" + bar.Date.Hour.ToString(), bar)
+			);
+			
+			first = source.First().Date;
+			last = source.Last().Date;
+		}
+	}
+}
---
+++ test/data/HourlySourceTest.cs
@@ -0,0 +1,82 @@
+﻿/***************************************************************************
+*   Copyright (C) 2011 by Denis M. Gabaydulin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License as        *
+*   published by the Free Software Foundation; either version 3 of the    *
+*   License, or (at your option) any later version.                       *
+*                                                                         *
+***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Mocks;
+using org.ontslab.data;
+using TSLab.Script;
+
+namespace org.ontslab.test.data {
+	[TestFixture]
+	public class HourlySourceTest {
+		private HourlySource source;
+		
+		[SetUp]
+		protected void init() {
+			source = new HourlySource(
+				new List<Bar>() {
+					{new Bar(0x000000, new DateTime(2011, 4, 13, 10, 0, 0), 100, 105, 99, 99, 1)},
+					{new Bar(0x000000, new DateTime(2011, 4, 13, 11, 0, 0), 102, 106, 101, 107, 1)},
+					{new Bar(0x000000, new DateTime(2011, 4, 13, 12, 0, 0), 108, 108, 106, 107, 1)},
+					{new Bar(0x000000, new DateTime(2011, 4, 13, 13, 0, 0), 99, 101, 84, 100, 21)},
+					{new Bar(0x000000, new DateTime(2011, 4, 13, 14, 0, 0), 101, 102, 98, 101.5, 1)},
+					{new Bar(0x000000, new DateTime(2011, 4, 16, 10, 0, 0), 102, 103, 100, 101.5, 21)}
+				}
+			);
+		}
+		
+		[Test]
+		public void testGetBar() {
+			// Bar is not ValueObject :-/
+			Assert.AreEqual(
+				new Bar(0x000000, new DateTime(2011, 4, 13, 12, 0, 0), 108, 108, 106, 107, 1).ToString(),
+				source.getBar(new DateTime(2011, 4, 13, 12, 0, 0)).ToString()
+			);
+		}
+		
+		[Test]
+		public void testGetPreviousBar() {
+			Assert.AreEqual(
+				new Bar(0x000000, new DateTime(2011, 4, 13, 10, 0, 0), 100, 105, 99, 99, 1).ToString(),
+				source.getPreviousBar(new DateTime(2011, 4, 13, 11, 0, 0)).ToString()
+			);
+			
+			Assert.AreEqual(
+				new Bar(0x000000, new DateTime(2011, 4, 13, 14, 0, 0), 101, 102, 98, 101.5, 1).ToString(),
+				source.getPreviousBar(new DateTime(2011, 4, 16, 10, 0, 0)).ToString()
+			);
+			
+			Assert.AreEqual(
+				null,
+				source.getPreviousBar(new DateTime(2011, 4, 13, 10, 0, 0))
+			);
+		}
+		
+		[Test]
+		public void testGetNextBar() {
+			Assert.AreEqual(
+				new Bar(0x000000, new DateTime(2011, 4, 13, 12, 0, 0), 108, 108, 106, 107, 1).ToString(),
+				source.getNextBar(new DateTime(2011, 4, 13, 11, 0, 0)).ToString()
+			);
+			
+			Assert.AreEqual(
+				new Bar(0x000000, new DateTime(2011, 4, 16, 10, 0, 0), 102, 103, 100, 101.5, 21).ToString(),
+				source.getNextBar(new DateTime(2011, 4, 13, 14, 0, 0)).ToString()
+			);
+			
+			Assert.AreEqual(
+				null,
+				source.getNextBar(new DateTime(2011, 4, 16, 10, 0, 0))
+			);
+		}
+	}
+}
