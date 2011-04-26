0c07ca77b3f125372476d5f434cfc19a070e2571
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-26 19:35:09 +0400

    + trivia limited queue

---
+++ src/util/LimitedQueue.cs
@@ -0,0 +1,32 @@
+﻿/***************************************************************************
+*   Copyright (C) 2011 by Denis M. Gabaydulin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License as        *
+*   published by the Free Software Foundation; either version 3 of the    *
+*   License, or (at your option) any later version.                       *
+*                                                                         *
+***************************************************************************/
+using System;
+using System.Collections.Generic;
+
+namespace org.ontslab.util {
+	/// <summary>
+	/// Description of LimitedQueue.
+	/// </summary>
+	public class LimitedQueue<T> : Queue<T> {
+    	private int limit = -1;
+    	
+    	 public LimitedQueue(int limit) : base(limit) {
+	        this.limit = limit;
+    	} 
+
+    	public new void Enqueue(T item) {
+        	if (this.Count >= this.limit) {
+            	this.Dequeue();
+        	}
+    		
+        	base.Enqueue(item);
+    	}
+	}
+}
