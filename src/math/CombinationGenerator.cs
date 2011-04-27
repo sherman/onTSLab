65d8b6867622ebdbdb8be7e6465f102bfd58859d
Denis M. Gabaidulin <gabaden@gmail.com>
2011-04-27 19:55:31 +0400

    + trivia combination with repetitions generator

---
+++ src/math/CombinationGenerator.cs
@@ -0,0 +1,35 @@
+﻿/***************************************************************************
+*   Copyright (C) 2011 by Denis M. Gabaydulin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License as        *
+*   published by the Free Software Foundation; either version 3 of the    *
+*   License, or (at your option) any later version.                       *
+*                                                                         *
+***************************************************************************/
+using System;
+using System.Collections.Generic;
+
+namespace org.ontslab.math {
+	/// <summary>
+	/// Description of CombinationGenerator.
+	/// </summary>
+	public static class CombinationGenerator {
+		public static void generate<T>(
+			IList<T> input,
+			IList<T> ouput,
+			IList<IList<T>> combinations,
+			int depth
+		) {
+			if (depth == 0) {
+				combinations.Add(new List<T>(ouput));
+			} else {
+				for (int i = 0; i < input.Count; i++) {
+					ouput.Add(input[i]);
+					generate(input, ouput, combinations, depth - 1);
+					ouput.RemoveAt(ouput.Count - 1);
+				}
+			}
+		}
+	}
+}
---
+++ test/math/CombinationGeneratorTest.cs
@@ -0,0 +1,41 @@
+﻿/***************************************************************************
+*   Copyright (C) 2011 by Denis M. Gabaydulin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License as        *
+*   published by the Free Software Foundation; either version 3 of the    *
+*   License, or (at your option) any later version.                       *
+*                                                                         *
+***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using org.ontslab.math;
+	
+namespace org.ontslab.test.math {
+	[TestFixture]
+	public class CombinationGeneratorTest {
+		[Test]
+		public void testGeneration() {
+			IList<IList<string>> generatedStrings = new List<IList<string>>();
+			CombinationGenerator.generate(
+				new List<string>(){"a", "b"},
+				new List<string>(),
+				generatedStrings,
+				2
+			);
+			
+			Assert.AreEqual(4, generatedStrings.Count);
+			
+			foreach (var generatedString in generatedStrings) {
+				Assert.AreEqual(2, generatedString.Count);
+			}
+			
+			Assert.AreEqual(new List<string>{"a", "a"}, generatedStrings[0]);
+			Assert.AreEqual(new List<string>{"a", "b"}, generatedStrings[1]);
+			Assert.AreEqual(new List<string>{"b", "a"}, generatedStrings[2]);
+			Assert.AreEqual(new List<string>{"b", "b"}, generatedStrings[3]);
+		}
+	}
+}
