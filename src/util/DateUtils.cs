d99ba0e847d7c5be90ef6292d5626bdaa20bcdae
Denis M. Gabaidulin <gabaden@gmail.com>
2011-11-18 17:46:51 +0300

    + usful date utils

---
+++ src/util/DateUtils.cs
@@ -0,0 +1,24 @@
+﻿/***************************************************************************
+*   Copyright (C) 2011 by Denis M. Gabaydulin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU Lesser General Public License as        *
+*   published by the Free Software Foundation; either version 3 of the    *
+*   License, or (at your option) any later version.                       *
+*                                                                         *
+***************************************************************************/
+using System;
+
+namespace org.ontslab.util
+{
+	/// <summary>
+	/// Description of DateUtils.
+	/// </summary>
+	public static class DateUtils {
+		public static DateTime alignDateToSeconds(DateTime date, int seconds) {
+			double alignPeriod = ((double)seconds) * 10000000; // win specific
+			long alignedTime = (long)(Math.Floor(date.Ticks / alignPeriod) * alignPeriod);
+			return new DateTime(alignedTime);
+		}
+	}
+}
