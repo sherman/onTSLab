3e00b237c78ce38b2d91a6239db493842023fd18
Denis M. Gabaidulin <gabaden@gmail.com>
2012-01-27 00:17:01 +0300

    + dist matrix method

--- src/util/MatrixUtils.cs
+++ src/util/MatrixUtils.cs
@@ -17,12 +17,34 @@ namespace org.ontslab.util
 	/// Description of MatrixUtils.
 	/// </summary>
 	public static class MatrixUtils {
+		public delegate T DistFunction<T>(T[,] matrix, int i, int j);
+		
 		public static T[,] asMatrix<T>(IList<T> data) {
 			T[,] matrix = new T[1,data.Count];
 			for (int i = 0; i < data.Count; i++) {
 				matrix[0,i] = data[i];
 			}
 			return matrix;
-		}                      
+		}
+		
+		public static T[,] getDistanceMatrix<T>(
+			T[,] matrix,
+			DistFunction<T> func,
+			T defaultValue
+		) {
+			long len = matrix.Length;
+
+			T[,] distances = new T[len,len];
+			
+			for (int i = 0; i < len; i++) {
+ 				for (int j = i + 1; j < len; j++) {
+					distances[i,j] = func(matrix, i, j);
+  					distances[j,i] = distances[j,i];
+				}
+				distances[i,i] = defaultValue;
+			}
+			
+			return distances;
+		}
 	}
 }
