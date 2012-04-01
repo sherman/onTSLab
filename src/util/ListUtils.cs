44a9f0e93c685332921522a271f5b8c6d8b86df4
Denis M. Gabaidulin <gabaden@gmail.com>
2012-04-01 19:32:22 +0400

    + asList() method

--- src/util/ListUtils.cs
+++ src/util/ListUtils.cs
@@ -52,5 +52,16 @@ namespace org.ontslab.util
 			
 			return list.Select(elt => eltToRanks[elt]).ToList();
 		}
+		
+		public static List<T> asList<T>(T[,] matrix) {
+			List<T> result = new List<T>(matrix.GetLength(0) * matrix.GetLength(1));
+			for (int i = 0; i < matrix.GetLength(0); i++) {
+				for (int j = 0; j < matrix.GetLength(1); j++) {
+					result.Add(matrix[i, j]);
+				}
+			}
+			
+			return result;
+		}
 	}
 }
