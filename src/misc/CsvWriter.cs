c2eece702e6bacab54459de455408a4d2ffd7da1
Denis M. Gabydulin <gabaden@gmail.com>
2017-08-13 11:59:54 +0300

    * csv writer

---
+++ src/misc/CsvWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace org.ontslab.misc {
+
+	public class CsvWriter {
+		public delegate void LineHandler(IList<string> parts);
+
+		private string fileName;
+		private string separator;
+		private StreamWriter writer;
+
+		public CsvWriter(string fileName, string separator) {
+			if (!File.Exists(fileName))
+				throw new FileNotFoundException("There is no such file:" + fileName);
+
+			this.fileName = fileName;
+			this.separator = separator;
+		}
+
+		public void write(List<List<string>> lines) {
+			writer = new StreamWriter(fileName);
+
+			foreach (var line in lines) {
+				writer.WriteLine(String.Join(separator, line));
+			}
+
+			writer.Close();
+		}
+	}
+}
