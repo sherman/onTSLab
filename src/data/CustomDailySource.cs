18fdbe7b8f63669453328523fb601ea79b68cdb7
Denis M. Gabydulin <gabaden@gmail.com>
2020-05-10 13:10:46 +0300

    + support gaps in next/prev bar methods

--- src/data/CustomDailySource.cs
+++ src/data/CustomDailySource.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using org.ontslab.misc;
 using TSLab.DataSource;
 using TSLab.Script;
 
@@ -10,6 +11,9 @@ namespace org.ontslab.data {
         private readonly IReadOnlyList<IDataBar> _original;
         private readonly int _skipBars;
         private readonly IDictionary<string, IDataBar> _newSourceBars = new Dictionary<string, IDataBar>();
+        private IDataBar first;
+        private IDataBar last;
+        private readonly Day period = new Day();
 
         public CustomDailySource(ISecurity original, int skipBars) : this(original.Bars, skipBars) {
         }
@@ -27,7 +31,7 @@ namespace org.ontslab.data {
             var max = double.MinValue;
             var volume = 0d;
 
-            for (int barIndex = _skipBars; barIndex < bars.Count; barIndex++) {
+            for (var barIndex = _skipBars; barIndex < bars.Count; barIndex++) {
                 var currentBar = bars[barIndex];
                 min = Math.Min(currentBar.Low, min);
                 max = Math.Max(currentBar.High, max);
@@ -49,13 +53,18 @@ namespace org.ontslab.data {
             for (var barIndex = 0; barIndex < _original.Count; barIndex++) {
                 var currentBar = _original[barIndex];
 
-                if (currentBar.Date.ToShortDateString() != dt) {
+                if (period.keyFromTime(currentBar.Date) != dt) {
                     if (bars.Count > _skipBars) {
                         var dayBar = CreateDayBar(bars);
-                        _newSourceBars.Add(dayBar.Date.ToShortDateString(), dayBar);
+                        _newSourceBars.Add(period.keyFromTime(dayBar.Date), dayBar);
+                        if (first == null) {
+                            first = dayBar;
+                        }
+
+                        last = dayBar;
                     }
 
-                    dt = currentBar.Date.ToShortDateString();
+                    dt = period.keyFromTime(currentBar.Date);
                     bars.Clear();
                     bars.Add(currentBar);
                 }
@@ -66,7 +75,12 @@ namespace org.ontslab.data {
 
             if (bars.Count > _skipBars) {
                 var dayBar = CreateDayBar(bars);
-                _newSourceBars.Add(dayBar.Date.ToShortDateString(), dayBar);
+                _newSourceBars.Add(period.keyFromTime(dayBar.Date), dayBar);
+                if (first == null) {
+                    first = dayBar;
+                }
+
+                last = dayBar;
             }
         }
 
@@ -75,15 +89,46 @@ namespace org.ontslab.data {
         }
 
         public IDataBar GetBar(DateTime dateTime) {
-            return _newSourceBars[dateTime.Date.ToShortDateString()];
+            return _newSourceBars[period.keyFromTime(dateTime.Date)];
         }
 
         public IDataBar GetPrevBar(DateTime dateTime) {
-            return _newSourceBars[dateTime.Date.AddDays(-1).Date.ToShortDateString()];
+            string previousBarKey;
+
+            var startBarDate = dateTime;
+
+            do {
+                startBarDate = startBarDate.Subtract(period.getTimeSpan());
+                previousBarKey = period.keyFromTime(startBarDate);
+
+                if (_newSourceBars.ContainsKey(previousBarKey)) {
+                    return _newSourceBars[previousBarKey];
+                }
+            } while (startBarDate >= first.Date);
+
+            return null;
         }
 
         public IDataBar GetNextBar(DateTime dateTime) {
-            return _newSourceBars[dateTime.Date.AddDays(1).Date.ToShortDateString()];
+            string nextBarKey;
+
+            DateTime startBarDate = dateTime;
+
+            do {
+                startBarDate = startBarDate.Add(period.getTimeSpan());
+                nextBarKey = period.keyFromTime(startBarDate);
+
+                if (_newSourceBars.ContainsKey(nextBarKey)) {
+                    return _newSourceBars[nextBarKey];
+                }
+            } while (startBarDate <= last.Date);
+
+
+            return null;
+        }
+
+        public IDataBar GetFirstBar() {
+            return first;
         }
     }
 }
--- test/data/CustomDailySourceTest.cs
+++ test/data/CustomDailySourceTest.cs
@@ -142,5 +142,51 @@ namespace org.ontslab.test.data {
                 source.GetPrevBar(new DateTime(2020, 1, 2)).ToString()
             );
         }
+        
+        [Test]
+        public void GetPrevBarWithGap() {
+            var source = new CustomDailySource(
+                new List<IDataBar> {
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 0, 0), 100, 105, 99, 99, 1)},
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 1, 0), 102, 106, 101, 107, 1)},
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 2, 0), 108, 108, 106, 107, 1)},
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 3, 0), 99, 101, 84, 100, 21)},
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 4, 0), 101, 102, 98, 101.5, 1)},
+                    {new DataBar(new DateTime(2020, 1, 5, 10, 0, 0), 101, 102, 98, 101.5, 1)},
+                    {new DataBar(new DateTime(2020, 1, 5, 10, 1, 0), 101, 103, 98, 101.5, 1)}
+                },
+                1
+            );
+
+            var expected = new DataBar(new DateTime(2020, 1, 1), 102, 108, 84, 101.5, 24);
+
+            Assert.AreEqual(
+                expected.ToString(),
+                source.GetPrevBar(new DateTime(2020, 1, 5)).ToString()
+            );
+        }
+        
+        [Test]
+        public void GetNextBarWithGap() {
+            var source = new CustomDailySource(
+                new List<IDataBar> {
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 0, 0), 100, 105, 99, 99, 1)},
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 1, 0), 102, 106, 101, 107, 1)},
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 2, 0), 108, 108, 106, 107, 1)},
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 3, 0), 99, 101, 84, 100, 21)},
+                    {new DataBar(new DateTime(2020, 1, 1, 10, 4, 0), 101, 102, 98, 101.5, 1)},
+                    {new DataBar(new DateTime(2020, 1, 5, 10, 0, 0), 101, 102, 98, 101.5, 1)},
+                    {new DataBar(new DateTime(2020, 1, 5, 10, 1, 0), 101, 103, 98, 101.5, 1)}
+                },
+                1
+            );
+
+            var expected = new DataBar(new DateTime(2020, 1, 5), 101, 103, 98, 101.5, 1);
+
+            Assert.AreEqual(
+                expected.ToString(),
+                source.GetNextBar(new DateTime(2020, 1, 1)).ToString()
+            );
+        } 
     }
 }
